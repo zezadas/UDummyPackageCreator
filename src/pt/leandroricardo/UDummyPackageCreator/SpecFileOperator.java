db7550ad585a69896f74a606e7fab89aed399d92
Leandro Ricardo <leandro.j.c.ricardo@gmail.com>
2014-09-02 15:50:18 +0100

    New class, that will operate the spec files.

---
+++ src/pt/leandroricardo/UDummyPackageCreator/SpecFileOperator.java
@@ -0,0 +1,50 @@
+package pt.leandroricardo.UDummyPackageCreator;
+
+public class SpecFileOperator {
+	/*
+	 * 	FileOperator:
+	 * 		This class will implement operations with spec files.
+	 * 		- Open an existing spec file
+	 * 		- Create a default spec file
+	 * 		- Edit a spec file using the user favourite text editor
+	 * 		- Build the package
+	 * 		- Extract the package
+	 * 		- Load package folder name (load information)
+	 * 		- Rebuild package
+	 * 		- Send file to launchpad PPA
+	 */
+	
+	public static void openSpecFile(){
+		
+	}
+	
+	public static void createDefaultSpecFile(){
+			
+	}
+	
+	public static void editSpecFile(){
+		
+	}
+	
+	public static void buidPackage(){
+		
+	}
+	
+	public static void extractPackage(){
+		
+	}
+	
+	public static String loadFolderName(){
+		return "";
+	}
+	
+	public static void rebuildPackage(){
+		
+	}
+	
+	public static void sendToLauchpadPPA(){
+		
+	}
+	
+
+}
